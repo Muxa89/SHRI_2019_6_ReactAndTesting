fdb80ca2476856d914643973ae5f20922b7dd1c3
Muxa89 <muxa.89@inbox.ru>
2019-10-04 18:54:11 +0400

    Added thunk.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
 
 import './App.scss';
 
-import reducer from './store/reducers/root.js';
+import reducer from '../store/reducers/root.js';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 console.log(store.getState());
 store.subscribe(() => console.log(store.getState()));
 
