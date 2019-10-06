43d84c6df3ef8a9879a1e1562aed3694c7d118a7
Muxa89 <muxa.89@inbox.ru>
2019-10-07 01:32:08 +0400

    Added simple integration tests for hermione.

--- test/hermione/simple.hermione.js
+++ test/hermione/simple.hermione.js
@@ -1,15 +1,48 @@
 /* eslint-disable no-undef */
-describe('123', () => {
-  it('123', function() {
+
+const assert = require('chai').assert;
+
+const timeout = ms => () =>
+  new Promise(resolve => {
+    setTimeout(() => resolve(), ms);
+  });
+
+describe('Root page', function() {
+  it('Shows repositories', function() {
     return this.browser
       .url('/')
-      .keys(['курс доллара к рублю', 'Enter'])
-      .isExisting('.converter-form')
-      .then(
-        () =>
-          new Promise(resolve => {
-            setTimeout(() => resolve(), 10000);
-          })
-      );
+      .isExisting('.InfoTable-Row')
+      .then(exists => {
+        assert.isTrue(exists);
+        return this.browser;
+      })
+      .getText('.InfoTable-Row > a > .InfoTable-Name > .InfoTable-Text')
+      .then(repoNames => {
+        assert.sameOrderedMembers(
+          repoNames,
+          ['Repo_1', 'Repo_2', 'Repo_3', 'Repo_4', 'Repo_5'],
+          'Names of repositories is different'
+        );
+      });
+  });
+
+  it('Moves to child dir when clicked on repository name', function() {
+    return this.browser
+      .url('/')
+      .isExisting('.InfoTable-Row')
+      .then(exists => {
+        assert.isTrue(exists);
+        return this.browser;
+      })
+      .$$('.InfoTable-Row > a')
+      .then(res => res[0])
+      .click()
+      .then(timeout(5000))
+      .getMeta()
+      .then(meta => console.log(meta))
+      // .then(url => {
+      //   assert.equal(url, 'http://localhost:8000/Repo_1');
+      // })
+      .then(timeout(5000));
   });
 });
