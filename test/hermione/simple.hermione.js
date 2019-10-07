adf1fb1e8036c9ba3f21e645c54b4f6d5b327900
Muxa89 <muxa.89@inbox.ru>
2019-10-08 01:00:29 +0400

    Added hermione integration test.

--- test/hermione/simple.hermione.js
+++ test/hermione/simple.hermione.js
@@ -2,19 +2,14 @@
 
 const assert = require('chai').assert;
 
-const timeout = ms => () =>
-  new Promise(resolve => {
-    setTimeout(() => resolve(), ms);
-  });
-
 describe('Root page', function() {
-  it('Shows repositories', function() {
+  it('Shows list of repositories', function() {
     return this.browser
       .url('/')
+      .waitForExist('.InfoTable-Row')
       .isExisting('.InfoTable-Row')
       .then(exists => {
         assert.isTrue(exists);
-        return this.browser;
       })
       .getText('.InfoTable-Row > a > .InfoTable-Name > .InfoTable-Text')
       .then(repoNames => {
@@ -26,23 +21,103 @@ describe('Root page', function() {
       });
   });
 
-  it('Moves to child dir when clicked on repository name', function() {
+  it('Redirects to child dir when clicked on repository name', function() {
     return this.browser
       .url('/')
+      .waitForExist('.InfoTable-Row')
+      .click('.InfoTable-Row > a')
+      .waitForExist('.InfoTable-Row')
+      .getUrl()
+      .then(url => {
+        assert.equal(url, 'http://localhost:8000/Repo_1/');
+      });
+  });
+
+  it('Not shows link to parent dir when in root', function() {
+    return this.browser
+      .url('/')
+      .waitForExist('.InfoTable-Row')
+      .getText('.InfoTable-Row')
+      .then(text => {
+        assert.notEqual(text[0], '../');
+      });
+  });
+});
+
+describe('Repository page', function() {
+  it('Shows files and folders list in repository', function() {
+    return this.browser
+      .url('/Repo_1')
+      .waitForExist('.InfoTable-Row')
       .isExisting('.InfoTable-Row')
       .then(exists => {
         assert.isTrue(exists);
-        return this.browser;
       })
-      .$$('.InfoTable-Row > a')
-      .then(res => res[0])
-      .click()
-      .then(timeout(5000))
-      .getMeta()
-      .then(meta => console.log(meta))
-      // .then(url => {
-      //   assert.equal(url, 'http://localhost:8000/Repo_1');
-      // })
-      .then(timeout(5000));
+      .getText('.InfoTable-Row > a > .InfoTable-Name > .InfoTable-Text')
+      .then(repoNames => {
+        assert.includeMembers(
+          repoNames,
+          ['Folder_1', 'Folder_2', 'Folder_3', 'File_1', 'File_2', 'File_3'],
+          'Names of repositories is different'
+        );
+      });
+  });
+
+  it('Shows link to parent dir in repository', function() {
+    return this.browser
+      .url('/Repo_1/')
+      .waitForExist('.InfoTable-Row')
+      .getText('.InfoTable-Row')
+      .then(text => {
+        assert.equal(text[0], '../');
+      });
+  });
+
+  it('Redirects to root when clicked in repositories first level', function() {
+    return this.browser
+      .url('/Repo_1/')
+      .waitForExist('.InfoTable-Row')
+      .click('.InfoTable-Row:nth-child(2) > a')
+      .waitForExist('.InfoTable-Row')
+      .getUrl()
+      .then(url => {
+        assert.equal(url, 'http://localhost:8000/');
+      });
+  });
+
+  it('Redirects one level deeper when clicked on folder in repositories first level', function() {
+    return this.browser
+      .url('/Repo_1/')
+      .waitForExist('.InfoTable-Row')
+      .click('.InfoTable-Row:nth-child(3) > a')
+      .waitForExist('.InfoTable-Row')
+      .getUrl()
+      .then(url => {
+        assert.equal(url, 'http://localhost:8000/Repo_1/tree/master/Folder_1');
+      })
+      .getText('.InfoTable-Row > a > .InfoTable-Name > .InfoTable-Text')
+      .then(repoNames => {
+        assert.includeMembers(
+          repoNames,
+          ['Folder_1', 'Folder_2', 'Folder_3', 'File_1', 'File_2', 'File_3'],
+          'Names of repositories is different'
+        );
+      });
+  });
+
+  it('Redirects to file detail page when clicked on file', function() {
+    return this.browser
+      .url('/Repo_1')
+      .waitForExist('.InfoTable-Row')
+      .click('.InfoTable-Row:nth-child(6) > a')
+      .waitForExist('.DetailPanel')
+      .isExisting('.DetailPanel')
+      .then(exists => {
+        assert.isTrue(exists);
+      })
+      .getUrl()
+      .then(url => {
+        assert.equal(url, 'http://localhost:8000/Repo_1/blob/master/File_1');
+      });
   });
 });
