dce0ad5480f1aabd05946061675ef861abc84ddc
Chepko Mikhail <MNChepko@sberbank.ru>
2020-06-23 11:52:35 +0400

    Sass fix.

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,16 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            }
+          }
+        ]
       },
       {
         test: /\.css$/,
