586d92404c6702d3878cfd568e0a4c3c6fc3d8d9
Mikhail Chepko <muxa.89@inbox.ru>
2020-07-14 10:40:04 +0400

    Removed logo. Returned RepositorySelector stub.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,14 +7,14 @@ import '../Selector/Selector.sass';
 
 const Header = (): React.ReactElement => (
   <div className='Header'>
-    <div className='Logo Header-Logo' />
-    {/*<div className='RepositorySelector Header-RepositorySelector Selector'>*/}
-    {/*  <div className='RepositorySelector-Text'>*/}
-    {/*    <span className='RepositorySelector-Repository'>Repository </span>*/}
-    {/*    <span className='RepositorySelector-Name'>Arc</span>*/}
-    {/*  </div>*/}
-    {/*  <div className='RepositorySelector-Arrow Selector-ArrowDown' />*/}
-    {/*</div>*/}
+    {/*<div className='Logo Header-Logo' />*/}
+    <div className='RepositorySelector Header-RepositorySelector Selector'>
+      <div className='RepositorySelector-Text'>
+        <span className='RepositorySelector-Repository'>Repository </span>
+        <span className='RepositorySelector-Name'>Arc</span>
+      </div>
+      <div className='RepositorySelector-Arrow Selector-ArrowDown' />
+    </div>
   </div>
 );
 export default Header;
