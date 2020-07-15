46589d99ce4060c905780b9a31f6cf46c7cecf72
Mikhail Chepko <muxa.89@inbox.ru>
2020-07-15 09:25:22 +0400

    Added nav links for files/branches mode selector.

--- src/components/ViewSelector/ViewSelector.tsx
+++ src/components/ViewSelector/ViewSelector.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
+import { useParams } from 'react-router';
+import 'src/components/ViewSelector/ViewSelector.sass';
+import Nav from 'react-bootstrap/Nav';
+import IURLParams from 'src/interfaces/IURLParams';
+import { getHref } from 'src/util/getHref';
 
-import './ViewSelector.sass';
-
-const ViewSelector = (): React.ReactElement => (
-  <div className='ViewSelector'>
-    <div className='ViewSelector-Item ViewSelector-Item_selected'>FILES</div>
-    <div className='ViewSelector-Item'>BRANCHES</div>
-  </div>
-);
+const ViewSelector = (): React.ReactElement => {
+  const { repositoryId, mode, hash, path }: IURLParams = useParams();
+  return (
+    <Nav variant='tabs'>
+      <Nav.Item>
+        <Nav.Link href={getHref({ repositoryId, mode: 'tree', hash, path })} active={mode === 'tree'}>
+          FILES
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href={getHref({ repositoryId, mode: 'branches', hash, path })} active={mode === 'branches'}>
+          BRANCHES
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+};
 export default ViewSelector;
